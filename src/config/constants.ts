f6a41137929aeab73be15e876eacab39e4641ea0
Jesus L <lopsanjesus@gmail.com>
2024-06-12 12:57:36 +0200

    Add constants

---
+++ src/config/constants.ts
@@ -0,0 +1,5 @@
+export const baseURL = "https://play.nextcaddy.com/api";
+export const tokenURL = "https://play.nextcaddy.com/api/login_check";
+
+export const tokenUsername = process.env.REACT_APP_TOKEN_USERNAME;
+export const tokenPassword = process.env.REACT_APP_TOKEN_PASSWORD;
