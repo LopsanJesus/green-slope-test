9629369c1c27c3a7fa9d1fd38f9b0a06fc2b96fe
Jesus L <lopsanjesus@gmail.com>
2024-06-12 12:57:48 +0200

    Add result type

---
+++ src/types/result.ts
@@ -0,0 +1,11 @@
+export interface ResultType {
+  id: number;
+  hcp: number;
+  scratch: number;
+  hits: string;
+  to_par_hcp: number;
+  to_par_scratch: number;
+  stableford_hcp: number;
+  stableford_scratch: number;
+  hole: number;
+}
